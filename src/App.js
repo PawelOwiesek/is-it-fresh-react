d26b0b0f459c88148e498f09a0ea7ef55729d5b4
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-01 14:56:40 +0200

    added removeProduct funcion and initialized [prod]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import ProductsList from "./ProductsList";
 import Clock from "./Clock";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      product: "Tomatoes",
+      timeIn: "2023-06-18 ",
+      timeOut: "2024-06-18 ",
+    },
+  ]);
+
+  const removeProduct = (id) => {
+    setProducts((products) => products.filter((product) => product.id !== id));
+  };
+
   return (
     <div>
       <Header title="Fresh check list." clock={<Clock />} />
       <section>
-        <Form />
+        <Form title="List of products and the expiration date." />
       </section>
       <section>
-        <ProductsList />
+        <ProductsList products={products} removeProduct={removeProduct} />
       </section>
     </div>
   );
