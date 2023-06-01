10b5a5b8f6d636e540b23e1f83cfc94f685e3b37
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-01 16:07:50 +0200

    added function responsible for adding new products

--- src/App.js
+++ src/App.js
@@ -18,11 +18,27 @@ function App() {
     setProducts((products) => products.filter((product) => product.id !== id));
   };
 
+  const addNewProduct = (newProduct, newTimeIn, newTimeOut) => {
+    setProducts((products) => [
+      ...products,
+      {
+        product: newProduct,
+        timeIn: newTimeIn,
+        timeOut: newTimeOut,
+        id: products.length ? products[products.length - 1].id + 1 : 1,
+      },
+    ]);
+    console.log(products);
+  };
+
   return (
     <div>
       <Header title="Fresh check list." clock={<Clock />} />
       <section>
-        <Form title="List of products and the expiration date." />
+        <Form
+          title="List of products and the expiration date."
+          addNewProduct={addNewProduct}
+        />
       </section>
       <section>
         <ProductsList products={products} removeProduct={removeProduct} />
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = ({ title }) => {
+const Form = ({ title, addNewProduct }) => {
+  const [newProduct, setNewProduct] = useState();
+  const [newTimeIn, setNewTimeIn] = useState();
+  const [newTimeOut, setNewTimeOut] = useState();
   const onFormSubmit = (e) => {
     e.preventDefault();
+    addNewProduct(newProduct.trim(), newTimeIn, newTimeOut);
   };
 
   return (
@@ -11,19 +16,37 @@ const Form = ({ title }) => {
       <p>
         <label className="form__products">
           Add a new product:
-          <input className="form__input--text " name="product" required />
+          <input
+            value={newProduct}
+            onChange={({ target }) => setNewProduct(target.value)}
+            className="form__input--text "
+            name="product"
+            required
+          />
         </label>
       </p>
       <p>
         <label className="form__date">
           Set the entry date:
-          <input className="form__input js-dateInput" type="date" required />
+          <input
+            value={newTimeIn}
+            onChange={({ target }) => setNewTimeIn(target.value)}
+            className="form__input js-dateInput"
+            type="date"
+            required
+          />
         </label>
       </p>
       <p>
         <label className="form__date">
           Expiration date:
-          <input className="form__input js-timeLeft" type="date" required />
+          <input
+            value={newTimeOut}
+            onChange={({ target }) => setNewTimeOut(target.value)}
+            className="form__input js-timeLeft"
+            type="date"
+            required
+          />
         </label>
       </p>
       <button className="button js-button">Add product to list.</button>
