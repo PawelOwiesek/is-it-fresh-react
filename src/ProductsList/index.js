7adb0c77bc2a7c1d3c8573a852f0becf25ecccdd
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-01 14:54:21 +0200

    added funcion rendering products list

--- src/ProductsList/index.js
+++ src/ProductsList/index.js
@@ -1,5 +1,34 @@
-import "./styles.css"
+import "./styles.css";
 
-const ProductsList = () => <ul className="js-productsList js-dates list"></ul>;
+const ProductsList = ({ products, removeProduct }) => {
+  return (
+    <ul className="list">
+      {products.map((product) => (
+        <>
+          <li className="list__item" key={product.id}>
+            {product.product}
+          </li>
+          <p className="list__item--dateIn">
+            Date in
+            <span className="list__item">{product.timeIn}</span>
+          </p>
+          <p className="list__item--exDate">
+            Date out
+            <span className="list__item">{product.timeOut}</span>
+          </p>
+          <p>
+            <span className="list__item--timeFlow">Time Flow</span>
+          </p>
+          <button
+            onClick={() => removeProduct(product.id)}
+            className="removeButton"
+          >
+            🗑
+          </button>
+        </>
+      ))}
+    </ul>
+  );
+};
 
 export default ProductsList;
