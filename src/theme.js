cfa36485137c9d58f8f86bf15360fc74fb807581
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-12 15:27:58 +0200

    created theme.js

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    teal: "#008080",
+    white: "#ffffff",
+    gray: "#767676",
+    darkGreen: "#283839",
+    black: "#000000",
+    red: "#f80101",
+    blueStone: "#006666",
+    nobel: "#b5acac",
+  },
+  breakpoints: {
+    mobile: 767,
+    tablet: 1024,
+  },
+};
